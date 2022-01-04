60ef3128fed7bfca0ba9dd3a2405465a67adcc01
LeixB <abone9999@gmail.com>
2022-01-04 17:40:51 +0100

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Cargo";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
@@ -11,14 +9,11 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        awesome = pkgs.awesome.override { lua = pkgs.lua5_3; };
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.sumneko-lua-language-server
-            awesome
-            pkgs.rofi
+          buildInputs = with pkgs; [
+            sumneko-lua-language-server
           ];
         };
       }
