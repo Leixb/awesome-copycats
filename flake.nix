cfbe061e9424e4b82fc2bc6956940ee7c2a216a8
LeixB <abone9999@gmail.com>
2022-01-06 20:25:36 +0100

    Add awesome and lain packages

--- flake.nix
+++ flake.nix
@@ -2,16 +2,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
+
+    awesome = {
+      url = "github:awesomewm/awesome/master";
+      flake = false;
+    };
+
+    lain = {
+      url = "github:lcpz/lain/master";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, awesome, lain }:
 
   {
     overlay = final: prev: {
       awesome-config = self.packages.${prev.system}.awesome-config;
+      awesome = self.packages.${prev.system}.awesome;
+      lain = self.packages.${prev.system}.lain;
     };
 
-  } // flake-utils.lib.eachDefaultSystem (system:
+  } // flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
@@ -31,6 +43,25 @@
             cp rc.lua $out
             cp -r themes $out
           '';
+        };
+
+        packages.lain = pkgs.lua53Packages.toLuaModule ( pkgs.stdenv.mkDerivation {
+          name = "lain";
+          src = lain;
+
+          installPhase = ''
+            mkdir -p $out
+            cp *.lua $out/
+            cp -r icons layout util widget $out
+          '';
+        });
+
+        packages.awesome = (pkgs.awesome.overrideAttrs (oldAttrs: rec {
+          src = awesome;
+        })).override {
+          lua = pkgs.lua5_3;
+          gtk3Support = true;
+          gtk3 = pkgs.gtk3;
         };
 
         defaultPackage = self.packages.${system}.awesome-config;
